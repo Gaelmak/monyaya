99c892c0d1f9b94cf2430be121a4bb6b19691b3e
12YBoke <yvesboke84@gmail.com>
2024-02-15 15:48:26 +0100

    fixed service page

--- front-end/src/app/(main-routes)/services/page.tsx
+++ front-end/src/app/(main-routes)/services/page.tsx
@@ -1,11 +1,18 @@
 import { Container } from "@/ui/components/container/container";
 import { FilterCourses } from "@/ui/modules/filter-courses/filter-courses";
+import prisma from '@/lib/prisma'
 
-export default function Home() {
+export default async function Home() {
+  const courses = await prisma.category.findMany({
+    include: {
+      courses: true
+    }
+  })
+
   return (
     <main>
       <Container variant="Glass-Effect" className="px-4 md:px-8 py-2 fixed left-0 right-0 drop-shadow-none" >
-        <FilterCourses/>
+        <FilterCourses CourseList={courses}/>
       </Container>
       <Container className="pt-[20vh] p-8">
         boke
