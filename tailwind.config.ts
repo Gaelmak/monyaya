1dfc9dfd65511c48a8bf985a94eb54e07efb66da
bertinselendo <contact@bertinselendo.com>
2024-08-28 19:30:10 +0100

    🛠️ fix: delete custom font-size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
-import type { Config } from 'tailwindcss'
-import { withUt } from 'uploadthing/tw'
+import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
 export default withUt({
-
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     container: {
@@ -20,98 +19,59 @@ export default withUt({
     },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
         // forest-green
-        primary: { 
-          50: '#f2fbf3',
-          55: '#f4f8f4' ,
-          100: '#e0f8e1',
-          200: '#c3efc6',
-          300: '#95e09c',
-          400: '#5fc969',
-          Default : '#39ae44',
-          600: '#2a9134',
-          700: '#24712c',
-          800: '#215a28',
-          900: '#1d4a23',
-          950: '#0b280f',
+        primary: {
+          50: "#f2fbf3",
+          55: "#f4f8f4",
+          100: "#e0f8e1",
+          200: "#c3efc6",
+          300: "#95e09c",
+          400: "#5fc969",
+          Default: "#39ae44",
+          600: "#2a9134",
+          700: "#24712c",
+          800: "#215a28",
+          900: "#1d4a23",
+          950: "#0b280f",
         },
         // storm-gray
         secondary: {
-          50: '#f7f8f8',
-          100: '#eeeef0',
-          200: '#d8dadf',
-          300: '#b6b9c3',
-          400: '#8f93a1',
-          Default: '#696d7d',
-          600: '#5b5f6e',
-          700: '#4a4c5a',
-          800: '#40424c',
-          900: '#383942',
-          950: '#25262c',
-        }, 
-        destructive: '#ff0035',
-        accent: '#f68e5f',
-        white : '#ffffff',
-        black : '#000f08',
-        // schist
-        gray : {
-          '50': '#f3f5f0',
-          '100': '#e6e8df',
-          '200': '#cfd3c3',
-          Default: '#a9b195',
-          '400': '#939d7e',
-          '500': '#778161',
-          '600': '#5c664a',
-          '700': '#494f3c',
-          '800': '#3c4133',
-          '900': '#35392e',
-          '950': '#1b1d16',
+          50: "#f7f8f8",
+          100: "#eeeef0",
+          200: "#d8dadf",
+          300: "#b6b9c3",
+          400: "#8f93a1",
+          Default: "#696d7d",
+          600: "#5b5f6e",
+          700: "#4a4c5a",
+          800: "#40424c",
+          900: "#383942",
+          950: "#25262c",
         },
-        white_powder : '#fafaf2'
-      },
-      fontSize: {
-        'display': ['4rem', {
-          lineHeight: '120%',
-          fontWeight: '700'
-        }],
-        'large-medium': ['3rem', {
-          lineHeight: '110%',  
-          fontWeight: '500'     
-        }],
-        'title-lg': ['2.5rem', {
-          lineHeight: '120%',
-          fontWeight: '700'
-        }],
-        'title-base': ['1.5rem', {
-          lineHeight: '120%',
-          fontWeight: '700'
-        }],
-        'title-sm': ['1rem', {
-          lineHeight: '120%',
-          fontWeight: '600'
-        }], 
-        'title-xs': ['0.85rem', {
-          lineHeight: '120%',
-          fontWeight: '600'
-        }],
-        'body-lg': ['1.5rem', {
-          lineHeight: '120%',
-          fontWeight: '600'
-        }],
-        'body-base': ['1rem', {
-          lineHeight: '120%',
-          fontWeight: '400'
-
-        }],
-        'body-sm': ['0.85rem', {
-          lineHeight: '120%',
-          fontWeight: '400'
-        }],
+        destructive: "#ff0035",
+        accent: "#f68e5f",
+        white: "#ffffff",
+        black: "#000f08",
+        // schist
+        gray: {
+          "50": "#f3f5f0",
+          "100": "#e6e8df",
+          "200": "#cfd3c3",
+          Default: "#a9b195",
+          "400": "#939d7e",
+          "500": "#778161",
+          "600": "#5c664a",
+          "700": "#494f3c",
+          "800": "#3c4133",
+          "900": "#35392e",
+          "950": "#1b1d16",
+        },
+        white_powder: "#fafaf2",
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -134,10 +94,5 @@ export default withUt({
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-  ],
- 
-})
-
-
+  plugins: [require("tailwindcss-animate")],
+});
