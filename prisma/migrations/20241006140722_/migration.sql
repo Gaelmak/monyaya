3fca232b98d81d1b90c1070e02f1f10ccb740b82
bertinselendo <contact@bertinselendo.com>
2024-10-06 19:58:28 +0100

    🔁 refactor: optimize database

---
+++ prisma/migrations/20241006140722_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "UserLesson" ADD COLUMN     "completedAt" TIMESTAMP(3);
---
+++ prisma/migrations/20241006144049_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "UserCourse" ADD COLUMN     "completedAt" TIMESTAMP(3);
