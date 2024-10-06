02134bebfb6290e33860142a523842b6836a1a85
bertinselendo <contact@bertinselendo.com>
2024-10-06 20:05:10 +0100

    🚀 feat: add api route

--- src/app/api/courses/[id]/route.ts
+++ src/app/api/courses/[id]/route.ts
@@ -18,6 +18,7 @@ export async function GET(
               select: {
                 id: true,
                 name: true,
+                email: true,
                 image: true,
                 firstName: true,
                 lastName: true,
--- src/app/api/lessons/[id]/complete/route.ts
+++ src/app/api/lessons/[id]/complete/route.ts
@@ -1,5 +1,39 @@
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
+// GET
+export async function GET(
+  req: Request & NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const searchParams = req.nextUrl.searchParams;
+  const userId = searchParams.get("userId");
+
+  if (!userId) {
+    return new Response("Missing required parameters: userId", {
+      status: 400,
+    });
+  }
+
+  try {
+    const updatedLesson = await prisma.userLesson.findUnique({
+      where: {
+        userId_lessonId: {
+          userId: userId,
+          lessonId: params.id,
+        },
+      },
+    });
+
+    return NextResponse.json(updatedLesson, { status: 202 });
+  } catch (error) {
+    console.error("Error updating lesson:", error);
+    return new Response(
+      "An unexpected error occurred while processing your request. Please review the data you provided and try again later.",
+      { status: 500 }
+    );
+  }
+}
 
 // UPDATE
 export async function PATCH(
