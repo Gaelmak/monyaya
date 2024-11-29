770a25829b4b230c3bcfe7c26e003b04c483d925
bertinselendo <contact@bertinselendo.com>
2024-11-29 14:05:15 +0100

    🚀 feat: add chat script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import ContextProvider from "@/components/context-provider";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Monyaya",
@@ -17,6 +18,14 @@ export default function RootLayout({
     <html lang="fr">
       <body className={GeistSans.className}>
         <ContextProvider>{children}</ContextProvider>
+        <Script
+          src="https://static.elfsight.com/platform/platform.js"
+          strategy="afterInteractive"
+        ></Script>
+        <div
+          className="elfsight-app-bbc42db3-5562-4490-a0e6-566aa2664599"
+          data-elfsight-app-lazy
+        ></div>
       </body>
     </html>
   );
